4db9ee8e403dafa9fa7244643939d0cd3132a37b
Victoria Chen <vic.chen128@gmail.com>
2017-04-24 20:50:40 -0400

    updated w/ corrections to method_detective.rb for modules 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,41 +4,41 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".swapcase
-# "iNvEsTiGaTiOn".swapcase!
+"iNvEsTiGaTiOn".swapcase
+"iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
-# "zom".insert(2,"o")
+"zom".insert(2,"o")
 # => “zoom”
 
-# "enhance".center(15)
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".upcase
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".concat(" suspects")
-# "the usual" << " suspects"
+"the usual".concat(" suspects")
+"the usual" << " suspects"
 #=> "the usual suspects"
 
-# " suspects".prepend("the usual")
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".chop
-# "The case of the disappearing last letter".chop!
+"The case of the disappearing last letter".chop
+"The case of the disappearing last letter".chop!
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".slice(0)*************
-# "The mystery of the missing first letter".slice!(0)
+"The mystery of the missing first letter".slice(0)
+"The mystery of the missing first letter".slice!(0)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".squeeze(" ")
-# "Elementary,    my   dear        Watson!".squeeze!(" ")
+"Elementary,    my   dear        Watson!".squeeze(" ")
+"Elementary,    my   dear        Watson!".squeeze!(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".ord
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".count("a")
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
