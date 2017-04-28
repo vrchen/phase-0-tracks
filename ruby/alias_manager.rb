8d30d9b965f4a3a55ab9e14ade48cfcb050a514d
Victoria Chen <vic.chen128@gmail.com>
2017-04-28 06:41:04 -0400

    started incomplete code for alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,31 @@
+#ask for the spy's name
+#store spy's name
+
+#assume name is 2 words separated by a space
+#split name with a space into two elements in an array
+#run reverse on the array
+
+#iterate through each letter in the spy's name
+#if the letter is a vowel, find the next vowel
+#if the letter is a consanant, find the next consanant
+
+#return altered name
+
+def fake_name_generator(str){
+	name = str.split
+	name.reverse!
+
+
+}
+
+
+
+def next_vowel(str) {
+	vowels = ["a","e","i","o","u"]
+	
+}
+
+def next_constant {
+
+}
+
