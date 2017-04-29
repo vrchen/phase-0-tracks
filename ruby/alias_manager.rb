f7d470356d02727b2dbafb86ccd012cd1b41a4a3
Victoria Chen <vic.chen128@gmail.com>
2017-04-30 01:30:21 +0300

    completed code for module 5.5

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,4 @@
-#ask for the spy's name
-#store spy's name
+#Release 0: Algorithm
 
 #assume name is 2 words separated by a space
 #split name with a space into two elements in an array
@@ -11,21 +10,57 @@
 
 #return altered name
 
-def fake_name_generator(str){
-	name = str.split
-	name.reverse!
-
-
-}
-
-
-
-def next_vowel(str) {
-	vowels = ["a","e","i","o","u"]
+def next_vowel_consonant(str) 
+  #create an array with only vowels or consonants
+	vowels = ["a","e","i","o","u","a"]
+	consonants = ("a".."z").to_a
+  consonants.reject! { |letter| vowels.include?(letter)}
+  consonants << "b" #for looping back to b when z is the letter
 	
-}
+	#convert input into an array of each letter
+	name_array = str.split("")
+	
+	#iterate through each letter
+	name_array.each_with_index do |letter,index|
+	  
+	  #if it is a vowel, find and replace with next
+	  if vowels.include?(letter)
+	    vowel_index = vowels.index(letter) + 1
+	    str[index] = vowels[vowel_index]
+	    
+ 	  #if it is a consonant, find and replace with next
+	  elsif consonants.include?(letter)
+	  	c_index = consonants.index(letter) + 1
+	    str[index] = consonants[c_index]
+	  end
+	end
+	str
+end
 
-def next_constant {
+def fake_name_generator(str)
+	name = str.downcase.split.reverse!
+	name.each do |name|
+    	next_vowel_consonant(name)
+    	name.capitalize!
+  	end
+	name = name.join(" ")
+end
 
-}
+
+#Release 1: Provide User Interface
+response = ""
+name_db = {}
+while response != "quit"
+	p "Enter a spy's name. Enter 'quit' to exit."
+	response = gets.chomp
+
+	if response != "quit"
+		name_db[response] = fake_name_generator(response)
+	end
+end
+
+#Release 2: Store the Aliases
+name_db.each do |old_name,new_name|
+	p "#{new_name} is actually #{old_name}."
+end
 
