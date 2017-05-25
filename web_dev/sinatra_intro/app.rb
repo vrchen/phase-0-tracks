0fcac7e2fed7384bf88851114958d34afba6e6f5
Victoria Chen <vic.chen128@gmail.com>
2017-05-25 16:43:34 -0400

    module 9.4, added the following routes: /contact, /great_job, and /:num1/plus/:num2, and also chang ed homepage to be able to take a student id as a query parameter and output a student profile

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
-require 'sinatra'
+require "sinatra"
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -8,9 +9,9 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} years old."
+# end
 
 # write a GET route with
 # route parameters
@@ -43,4 +44,42 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+# Release 0
+
+# 1. A /contact route that displays an address
+get '/contact' do
+	"The address is: <br>1234 Street Rd <br>Great City, CA 99999"
+end
+
+# 2. A /great_job route that can take a person's name as a query parameter
+# and say "Good job, [person's name]"
+# if query parameter not present, then say "Good job!"
+get '/great_job' do
+	person_name = params[:name]
+	if person_name
+		"Good job, #{person_name}!"
+	else
+		"Good job!"
+	end
+end
+
+# 3. a route that uses route parameters to add two numbers and respond w the result
+get '/:num1/plus/:num2' do
+	first_num = params[:num1].to_i
+	second_num = params[:num2].to_i
+	sum = first_num + second_num
+	"#{first_num} plus #{second_num} is #{sum}"
+end
+
+# 4. Optional bonus: make homepage a route that allows users to search the database by ID
+get '/' do
+	student = db.execute("SELECT * FROM students WHERE id=?",params[:id])[0]
+    response = "Search Result:<br>"
+    response << "ID: #{student['id']}<br>"
+    response << "Name: #{student['name']}<br>"
+    response << "Age: #{student['age']}<br>"
+    response << "Campus: #{student['campus']}<br>"
+    response
 end
