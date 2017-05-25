f57a1e86a6bd99ba26c1baf4d4b19ed4837a8f19
Victoria Chen <vic.chen128@gmail.com>
2017-05-25 19:41:15 -0400

    added a routes /locations /add_location and /delete_location that creates a campuses table if it doesn't already exist and add/deletes locations based on inputs from locations.erb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -25,3 +26,26 @@ post '/students' do
 end
 
 # add static resources
+
+
+# module 9.5
+
+get '/locations' do
+
+  # create a new table containing a de-duplicated list of locations from students
+  db.execute("CREATE TABLE IF NOT EXISTS campuses AS SELECT DISTINCT campus FROM students")
+  @locations = db.execute("SELECT * FROM campuses")
+  erb :locations
+end
+
+post '/add_location' do
+	location = params['location']
+	db.execute("INSERT INTO campuses (campus) VALUES (?)", [location])
+	redirect '/locations'
+end
+
+post '/delete_location' do
+	location = params['location']
+	db.execute("DELETE FROM campuses WHERE campus=?",[location])
+	redirect '/locations'
+end
